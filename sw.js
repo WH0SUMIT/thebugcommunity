0452ac2a688ab890e7bae97ca4a07f78f80a49cc
wh0sumit <sumikumar1608@gmail.com>
2021-10-30 03:28:24 +0530

    Cache in pwa app

--- sw.js
+++ sw.js
@@ -1,13 +1,50 @@
+const staticCacheName = "site-static";
+const dynamicCache = "site-dynamic-v1";
+const assets = [
+  "/",
+  "/index.html",
+  "/js/main.js",
+  "/js/pwa.js",
+  "/css/style.css",
+  "/assets/img/about.png",
+  "/assets/img/design.png",
+  "/assets/img/hero-img.png",
+  "/assets/img/logo.png",
+  "/assets/img/logo-2.png",
+  "https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap",
+  "https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css",
+  "/Pages/notfound.html",
+];
+
 self.addEventListener("install", (evt) => {
-  console.log("helo");
+  //   console.log('helo');
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log("done cache");
+      cache.addAll(assets);
+    })
+  );
 });
 
 //Activate servie worker
 self.addEventListener("activate", (evt) => {
-  console.log("ddd");
+  //   console.log('ddd');
 });
 
 //Fetch Event
 self.addEventListener("fetch", (evt) => {
-  console.log("fetc", evt);
+  evt.respondWith(
+    caches.match(evt.request).then((cacheRes) => {
+      console.log("nono");
+      return (
+        cacheRes ||
+        fetch(evt.request).then((fetchRes) => {
+          return caches.open(dynamicCache).then((cache) => {
+            cache.put(evt.request.url, fetchRes.clone());
+            return fetchRes;
+          });
+        })
+      );
+    })
+  );
 });
