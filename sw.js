5d878116f49b2f048ebef670bfd4afb8460419ba
wh0sumit <sumikumar1608@gmail.com>
2021-10-30 03:39:59 +0530

    Cache in pwa app

--- sw.js
+++ sw.js
@@ -14,6 +14,7 @@ const assets = [
   "https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap",
   "https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css",
   "/Pages/notfound.html",
+  "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3684.7642348183103!2d75.76031841495913!3d22.5505028851942!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x90e60767ffa5196a!2sThe%20Bug%20Community!5e0!3m2!1sen!2sin!4v1635234793517!5m2!1sen!2sin",
 ];
 
 self.addEventListener("install", (evt) => {
@@ -34,17 +35,20 @@ self.addEventListener("activate", (evt) => {
 //Fetch Event
 self.addEventListener("fetch", (evt) => {
   evt.respondWith(
-    caches.match(evt.request).then((cacheRes) => {
-      console.log("nono");
-      return (
-        cacheRes ||
-        fetch(evt.request).then((fetchRes) => {
-          return caches.open(dynamicCache).then((cache) => {
-            cache.put(evt.request.url, fetchRes.clone());
-            return fetchRes;
-          });
-        })
-      );
-    })
+    caches
+      .match(evt.request)
+      .then((cacheRes) => {
+        console.log("nono");
+        return (
+          cacheRes ||
+          fetch(evt.request).then((fetchRes) => {
+            return caches.open(dynamicCache).then((cache) => {
+              cache.put(evt.request.url, fetchRes.clone());
+              return fetchRes;
+            });
+          })
+        );
+      })
+      .catch(() => caches.match("/Pages/notfound.html"))
   );
 });
